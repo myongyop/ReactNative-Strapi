125f1dc1008c0e95b531efe7d82318cbeb899c5f
tanberine <mylee@tanberine.com>
2023-06-11 14:24:21 +0900

    Source Init.

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '08ea840f1f1cbde126ededa238d1e507'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b17800a7-4752-47ae-9acb-cc188fc2777f'
+};
